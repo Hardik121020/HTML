3128a621e0d6ea0a271cddf99eb1724451d5e9c2
Hardik121020 <55340731+Hardik121020@users.noreply.github.com>
2020-09-13 00:31:55 +0530

    Create function1.js

---
+++ JavaScript/JS/function1.js
@@ -0,0 +1,13 @@
+function openMe()
+{
+	x = document.getElementById("change1");
+
+	x.style.display="block";
+}
+
+function closeMe()
+{
+	x = document.getElementById("change1");
+
+	x.style.display="none";
+}
