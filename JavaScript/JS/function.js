4e2f84928d8fd04fe8c370c4a1c820a48ebb9a67
Hardik121020 <55340731+Hardik121020@users.noreply.github.com>
2020-09-12 23:38:28 +0530

    Add files via upload

---
+++ JavaScript/JS/function.js
@@ -0,0 +1,19 @@
+function setdate()
+{
+	document.getElementById('date').innerHTML=Date();
+}
+
+function name()
+{
+	document.write('Hardik Bansal');
+}
+
+function secret()
+{
+	document.write('Do your own business');
+}
+
+function message(mes)
+{
+	document.getElementById('output').innerHTML = mes + "event";
+} 
